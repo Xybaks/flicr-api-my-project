3c92b9ce240dc51239696ce20c8b1369f797c4c0
Xybaks <nikolai.berestevich@gmail.com>
2021-04-27 01:35:58 +0300

    I'm going to sleep. Alert was refactored

--- src/common/components/MyAlert/MyAlert.tsx
+++ src/common/components/MyAlert/MyAlert.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
 import Snackbar from '@material-ui/core/Snackbar';
 import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
@@ -34,9 +33,6 @@ export default function MyAlert(props: MyAlertPropsType) {
                 message= {props.errorMessage}
                 action={
                     <React.Fragment>
-                        <Button color="secondary" size="small" onClick={handleClose}>
-                            UNDO
-                        </Button>
                         <IconButton size="small" aria-label="close" color="inherit" onClick={handleClose}>
                             <CloseIcon fontSize="small"/>
                         </IconButton>
