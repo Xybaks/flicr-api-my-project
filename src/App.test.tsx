be1f9b2dd8c2c93cb7fe8c7c4c00dff45fe7dcc2
Xybaks <nikolai.berestevich@gmail.com>
2021-04-27 17:19:05 +0300

    App.test.tsx - small refactoring

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,17 +1,10 @@
 import React from 'react';
 import {render, screen} from '@testing-library/react';
 import App from './App';
-import {HashRouter} from "react-router-dom";
-import {Provider} from "react-redux";
-import {store} from "./state/reduxStore";
 
-test('renders HEADER', () => {
-    render(
-        <HashRouter>
-            <Provider store={store}>
-                <App/>
-            </Provider>
-        </HashRouter>);
+
+test('App renders Header', () => {
+    render(<App/>)
     const linkElement = screen.getByText(/IMAGE FINDER/i);
     expect(linkElement).toBeInTheDocument();
 });
